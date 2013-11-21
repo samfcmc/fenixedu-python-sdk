eafefefabc9b7e024c1703d78b26a7dca1837456
Samuel Coelho <samuelfcmc@gmail.com>
2013-11-21 20:41:19 +0000

    Implemented get person. Closes #3

--- fenix.py
+++ fenix.py
@@ -3,7 +3,6 @@ FENIX SDK source code :)
 """
 
 import requests
-import urllib
 
 class FenixAPI(object):
 	def __init__(self):
@@ -15,20 +14,22 @@ class FenixAPI(object):
 		self.oauth_endpoint = 'oauth/'
 		self.api_endpoint = 'api/fenix/'
 		self.api_version = 1
+		self.access_token = ''
+		self.error_key = 'error'
 
-	def get_api_url():
-		return self.base_url + api_endpoint + 'v' + self.api_version
+	def _get_api_url(self):
+		return self.base_url + self.api_endpoint + 'v' + str(self.api_version)
 
-	def _request(self, endpoint, req_params=None):
-		url = self.base_url + endpoint
+	def _request(self, url, req_params=None):
 		r = requests.get(url, params=req_params)
 		print(r.url)
 		return r
 
 	def _api_request(self, endpoint, req_params=None):
 		req_params = req_params or {}
+		url = self._get_api_url() + '/' + endpoint
 		req_params['access_token'] = self.access_token
-		self._request(endpoint, req_params)
+		return self._request(url, req_params)
 
 	def get_authentication_url(self):
 		url = self.base_url + self.oauth_endpoint + 'userdialog?client_id=' + self.client_id + '&redirect_uri=' + self.redirect_uri
@@ -40,7 +41,7 @@ class FenixAPI(object):
 		r_headers = {'content-type' : 'application/x-www-form-urlencoded'}
 		r = requests.post(url, params = r_params, headers = r_headers)
 		r_object = r.json()
-		if 'error' in r_object:
+		if self.error_key in r_object:
 			print('Error tryng to get an access token')
 			print(r_object)
 		else:
@@ -56,4 +57,9 @@ class FenixAPI(object):
 
 	def get_token_expires(self):
 		return self.exprires
+
+	""" API methods """
+	def get_person(self):
+		r = self._api_request('person')
+		return r.json()
 
