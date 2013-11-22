177317a83f23559b1caed76c9551d872e5984345
Samuel Coelho <samuelfcmc@gmail.com>
2013-11-22 00:56:54 +0000

    Added a setup file to install the sdk

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Script to install FenixEdu API python SDK"""
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='FenixEduPythonAPISDK',
+		version='1.0',
+		description='FenixEdu API SDK for python',
+		author='Samuel Coelho',
+		author_email='samuelfcmc@gmail.com',
+		url='https://github.com/samfcmc/fenixedu-python-sdk',
+		py_modules=['fenix']
+		)
