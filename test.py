8c8307ff5313b1f6c69d976853f763daf2aece0c
Samuel Coelho <samuelfcmc@gmail.com>
2013-12-26 16:50:07 +0000

    Test now calls a public endpoint first

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import fenix
 api = fenix.FenixAPISingleton()
 print('Testing Fenix API SDK Python')
 auth_url = api.get_authentication_url()
+print(api.get_space('2465311230082'))
 print(auth_url)
 api.set_code(sys.argv[1])
 print('Access token: ' + api.get_access_token())
@@ -13,4 +14,3 @@ print('Refresh token: ' + api.get_refresh_token())
 api._refresh_access_token()
 print('New access token: ' + api.get_access_token())
 
-print(api.get_space('2465311230082'))
