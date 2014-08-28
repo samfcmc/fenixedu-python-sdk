83bab67ebd9d87ca8b2ee45ea6c49634732bc241
Samuel <samuelfcmc@gmail.com>
2014-08-28 23:28:34 +0100

    Changed version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup, Extension
 
 setup(name='fenixedu',
-		version='2.0.1',
+		version='1.0.0',
 		description='FenixEdu API SDK for python',
 		author='Samuel Coelho',
 		author_email='samuelfcmc@gmail.com',
