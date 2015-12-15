e7035094822777479be3f40c28dd1ecc52baab49
rubenmmarques <rfmm.176354@gmail.com>
2015-12-15 21:06:12 +0000

    Update user.py

--- fenixedu/user.py
+++ fenixedu/user.py
@@ -2,9 +2,8 @@
 """ User: """
 class User(object):
   def __init__(self, access_token = None, refresh_token = None,
-                token_expires = None, code = None):
+                token_expires = None):
     self.access_token = access_token
     self.refresh_token = refresh_token
     self.token_expires = token_expires
-    self.code = code
 
